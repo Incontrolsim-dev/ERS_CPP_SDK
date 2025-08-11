d38f2f246a9e9a1bb97bc2aba49f7d402c85aab3
Mark Oostveen <mark.oostveen@incontrolsim.com>
2025-08-11 09:57:30 +0200

    Mirror from GitLab and set SDK_URL for 0.5.2

---
+++ RemoteSetup.cmake
@@ -0,0 +1,15 @@
+cmake_minimum_required (VERSION 3.20)
+
+macro(ERS_Download_SDK TAG)
+    Include(FetchContent)
+    FetchContent_Declare(
+        ERS_ENGINE
+        GIT_REPOSITORY https://github.com/incontrolsim/ERS_CPP_SDK.git
+        GIT_TAG        ${TAG}
+        )
+    FetchContent_GetProperties(ERS_ENGINE)
+    if(NOT ERS_ENGINE_POPULATED)
+        message(STATUS "Downloading ERS SDK package")
+        FetchContent_Populate(ERS_ENGINE)
+    endif()
+endmacro()
